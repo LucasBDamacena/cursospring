5135a89a33dc7be958b6b006a3875f8d049c2781
LucasBDamacena <lucasbusato03@gmail.com>
2019-07-16 22:41:48 -0300

    Ajustes no endpoint /categorias/{id}

---
+++ src/main/java/com/lucasbdamacena/cursospring/resources/exceptions/StandardError.java
@@ -0,0 +1,50 @@
+package com.lucasbdamacena.cursospring.resources.exceptions;
+
+import java.io.Serializable;
+
+public class StandardError implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	private Integer code;
+	private String msg;
+	private long timeStamp;
+	
+	
+	public StandardError(Integer code, String msg, long timeStamp) {
+		super();
+		this.code = code;
+		this.msg = msg;
+		this.timeStamp = timeStamp;
+	}
+
+
+	public Integer getCode() {
+		return code;
+	}
+
+
+	public void setCode(Integer code) {
+		this.code = code;
+	}
+
+
+	public String getMsg() {
+		return msg;
+	}
+
+
+	public void setMsg(String msg) {
+		this.msg = msg;
+	}
+
+
+	public long getTimeStamp() {
+		return timeStamp;
+	}
+
+
+	public void setTimeStamp(long timeStamp) {
+		this.timeStamp = timeStamp;
+	}	
+	
+}
