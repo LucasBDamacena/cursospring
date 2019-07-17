02add1849f9ae9cf5aaaca9533d912d7b5883725
LucasBDamacena <lucasbusato03@gmail.com>
2019-07-16 21:45:47 -0300

    Criado operação de instanciação

--- src/main/java/com/lucasbdamacena/cursospring/CursospringApplication.java
+++ src/main/java/com/lucasbdamacena/cursospring/CursospringApplication.java
@@ -1,13 +1,31 @@
 package com.lucasbdamacena.cursospring;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursospringApplication {
+import com.lucasbdamacena.cursospring.domain.Categoria;
+import com.lucasbdamacena.cursospring.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class CursospringApplication implements CommandLineRunner {
+
+	@Autowired
+	CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursospringApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria c1 = new Categoria(null, "Informática");
+		Categoria c2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(c1,c2));
 	}
 
 }
