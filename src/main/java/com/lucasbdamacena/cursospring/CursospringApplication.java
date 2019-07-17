b283e784308df228f28065cb99fae9951a785293
LucasBDamacena <lucasbusato03@gmail.com>
2019-07-16 22:10:49 -0300

    Produtos e associação Muitos para Muitos

--- src/main/java/com/lucasbdamacena/cursospring/CursospringApplication.java
+++ src/main/java/com/lucasbdamacena/cursospring/CursospringApplication.java
@@ -8,13 +8,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.lucasbdamacena.cursospring.domain.Categoria;
+import com.lucasbdamacena.cursospring.domain.Produto;
 import com.lucasbdamacena.cursospring.repositories.CategoriaRepository;
+import com.lucasbdamacena.cursospring.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class CursospringApplication implements CommandLineRunner {
 
 	@Autowired
 	CategoriaRepository categoriaRepository;
+	
+	@Autowired
+	ProdutoRepository produtoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursospringApplication.class, args);
@@ -25,7 +30,19 @@ public class CursospringApplication implements CommandLineRunner {
 		Categoria c1 = new Categoria(null, "Informática");
 		Categoria c2 = new Categoria(null, "Escritório");
 		
+		Produto p1 = new Produto(null, "Computador", 2000);
+		Produto p2 = new Produto(null, "Impressora", 800);	
+		Produto p3 = new Produto(null, "Mouse", 80);
+		
+		c1.setProdutos(Arrays.asList(p1,p2,p3));
+		c2.setProdutos(Arrays.asList(p2));
+		
+		p1.setCategorias(Arrays.asList(c1));
+		p2.setCategorias(Arrays.asList(c1,c2));
+		p3.setCategorias(Arrays.asList(c1));
+				
 		categoriaRepository.saveAll(Arrays.asList(c1,c2));
+		produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
 	}
 
 }
