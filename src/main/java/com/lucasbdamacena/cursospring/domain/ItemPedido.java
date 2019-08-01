ce9ad8686ae0c8b2008b9e083067f3013bbce15e
LucasBDamacena <lucasbusato03@gmail.com>
2019-07-31 21:27:59 -0300

    Subtotal e total do Pedido

--- src/main/java/com/lucasbdamacena/cursospring/domain/ItemPedido.java
+++ src/main/java/com/lucasbdamacena/cursospring/domain/ItemPedido.java
@@ -8,9 +8,9 @@ import javax.persistence.Entity;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
-public class ItemPedido implements Serializable{	
+public class ItemPedido implements Serializable {
 	private static final long serialVersionUID = 1L;
-
+	
 	@JsonIgnore
 	@EmbeddedId
 	private ItemPedidoPK id = new ItemPedidoPK();
@@ -19,17 +19,21 @@ public class ItemPedido implements Serializable{
 	private Integer quantidade;
 	private Double preco;
 	
-	public ItemPedido() {}
-
+	public ItemPedido() {
+	}
 	public ItemPedido(Pedido pedido, Produto produto, Double desconto, Integer quantidade, Double preco) {
 		super();
-		this.id.setPedido(pedido);
-		this.id.setProduto(produto);
+		id.setPedido(pedido);
+		id.setProduto(produto);
 		this.desconto = desconto;
 		this.quantidade = quantidade;
 		this.preco = preco;
 	}
-	
+
+	public double getSubTotal() {
+		return (preco - desconto) * quantidade;
+	}
+
 	@JsonIgnore
 	public Pedido getPedido() {
 		return id.getPedido();
@@ -38,39 +42,31 @@ public class ItemPedido implements Serializable{
 	public Produto getProduto() {
 		return id.getProduto();
 	}
-
+	
 	public ItemPedidoPK getId() {
 		return id;
 	}
-
 	public void setId(ItemPedidoPK id) {
 		this.id = id;
 	}
-
 	public Double getDesconto() {
 		return desconto;
 	}
-
 	public void setDesconto(Double desconto) {
 		this.desconto = desconto;
 	}
-
 	public Integer getQuantidade() {
 		return quantidade;
 	}
-
 	public void setQuantidade(Integer quantidade) {
 		this.quantidade = quantidade;
 	}
-
 	public Double getPreco() {
 		return preco;
 	}
-
 	public void setPreco(Double preco) {
 		this.preco = preco;
 	}
-
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -78,7 +74,6 @@ public class ItemPedido implements Serializable{
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		return result;
 	}
-
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -94,6 +89,6 @@ public class ItemPedido implements Serializable{
 		} else if (!id.equals(other.id))
 			return false;
 		return true;
-	}	
-
+	}
+	
 }
