2b27de78da12090f4b9eee3f23b960b7b4db9534
LucasBDamacena <lucasbusato03@gmail.com>
2019-07-31 22:33:26 -0300

    Inserindo Pedido

--- src/main/java/com/lucasbdamacena/cursospring/resources/PedidoResource.java
+++ src/main/java/com/lucasbdamacena/cursospring/resources/PedidoResource.java
@@ -1,13 +1,21 @@
 package com.lucasbdamacena.cursospring.resources;
 
+import java.net.URI;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.lucasbdamacena.cursospring.domain.Categoria;
 import com.lucasbdamacena.cursospring.domain.Pedido;
+import com.lucasbdamacena.cursospring.dto.CategoriaDTO;
 import com.lucasbdamacena.cursospring.services.PedidoService;
 
 @RestController
@@ -15,12 +23,22 @@ import com.lucasbdamacena.cursospring.services.PedidoService;
 public class PedidoResource {
 
 	@Autowired
-	private PedidoService pedidoService;
+	private PedidoService service;
 	
 	@RequestMapping(value="/{id}", method= RequestMethod.GET)
 	public ResponseEntity<Pedido> find(@PathVariable Integer id){
-		Pedido pedido = pedidoService.find(id);		
+		Pedido pedido = service.find(id);		
 		return ResponseEntity.ok().body(pedido);
+	}
+	
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> insert(@Valid @RequestBody Pedido obj){		
+		obj = service.insert(obj);	
+		
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(obj.getId()).toUri(); 
+		
+		return ResponseEntity.created(uri).build();
 	}
 
 }
--- src/main/java/com/lucasbdamacena/cursospring/services/PedidoService.java
+++ src/main/java/com/lucasbdamacena/cursospring/services/PedidoService.java
@@ -1,11 +1,18 @@
 package com.lucasbdamacena.cursospring.services;
 
+import java.util.Arrays;
+import java.util.Date;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.lucasbdamacena.cursospring.domain.ItemPedido;
+import com.lucasbdamacena.cursospring.domain.PagamentoComBoleto;
 import com.lucasbdamacena.cursospring.domain.Pedido;
+import com.lucasbdamacena.cursospring.domain.enums.EstadoPagamento;
+import com.lucasbdamacena.cursospring.repositories.ItemPedidoRepository;
+import com.lucasbdamacena.cursospring.repositories.PagamentoRepository;
 import com.lucasbdamacena.cursospring.repositories.PedidoRepository;
 import com.lucasbdamacena.cursospring.services.exceptions.ObjectNotFoundException;
 
@@ -15,9 +22,42 @@ public class PedidoService {
 	@Autowired
 	private PedidoRepository repo;
 	
+	@Autowired
+	private BoletoService boletoService;
+	
+	@Autowired
+	private PagamentoRepository pagamentoRepository;
+	
+	@Autowired
+	private ProdutoService produtoService;
+	
+	@Autowired
+	private ItemPedidoRepository itemPedidoRepository;
+	
 	public Pedido find(Integer id) {
 		Optional<Pedido> cliente = repo.findById(id);
 		return cliente.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado. ID: "+ id + 
 				"Tipo: " + Pedido.class.getName()));
+	}
+	
+	public Pedido insert(Pedido obj) {
+		obj.setId(null);
+		obj.setInstante(new Date());
+		obj.getPagamento().setEstado(EstadoPagamento.PENDENTE);
+		obj.getPagamento().setPedido(obj);
+		if(obj.getPagamento() instanceof PagamentoComBoleto) {
+			PagamentoComBoleto pagto = (PagamentoComBoleto) obj.getPagamento();
+			boletoService.preencherPagamentoComBoleto(pagto,obj.getInstante());
+		}
+		
+		obj = repo.save(obj);
+		pagamentoRepository.save(obj.getPagamento());
+		for(ItemPedido ip : obj.getItens()) {
+			ip.setDesconto(0.0);
+			ip.setPreco(produtoService.find(ip.getProduto().getId()).getPreco());
+			ip.setPedido(obj);			
+		}
+		itemPedidoRepository.saveAll(obj.getItens());
+		return obj;		
 	}
 }
